6e8264ffd8e7558b4fcea8e1f883c6399f9ed1a1
Emanstone <127052283+Emanstone@users.noreply.github.com>
2023-03-31 11:28:54 +0100

    Update lottery_app.py
    On this lottery application, USER INPUT NUMBER is compared to the WINNING NUMBER which is also compared to the RANDOMLY GENERATED NUMBER.  User only wins if these numbers are same.

--- lottery_app.py
+++ lottery_app.py
@@ -1,19 +1,24 @@
-#list of Objects; range, if, else, sys
-
+#list of Objects; random, if, else, sys
+import random
 import sys
 
-winning_number = 30
+winning_number = 3
 
 # Display;
 print("Welcome to the lottery game!")
-print("Numbers available: 0 - 50")
+print("Numbers available: 0 - 5")
+user_name = input("Enter your name: ")
+print("Welcome",user_name)
 
 # Getting user input
 user_input = input("Enter number: ")
+# Generate a random integer between 1 and 5(inclusive)
+randnum = random.randint(1, 5)
+# print(randnum
 
 # Converting the input to integers
 user_num = int(user_input)
-if int(user_num) > 50:
+if int(user_num) > 5:
     print("Disqualified! Chosen number beyond specified samples")
     sys.exit() 
 
@@ -21,7 +26,7 @@ if int(user_num) > 50:
 print("Your chosen number is:", user_num)
 
 # Checking if the user won
-if winning_number is user_num:
-    print("Congratulations! You won!")
+if winning_number == user_num == randnum:
+    print("Congratulations!", user_name, "You won!")
 else:
-    print("Sorry, you did not win. Better luck next time!")
+    print("Sorry",user_name, "you did not win. Better luck next time!")
